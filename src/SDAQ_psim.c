d6d6a7b34d755a01b1dfbb03b7a7fec5694e31cc
Sam Harry Tzavaras <fantomsam@hotmail.com>
2019-12-15 23:57:40 +0200

    Update

--- src/SDAQ_psim.c
+++ src/SDAQ_psim.c
@@ -422,16 +422,16 @@ void * pseudo_SDAQ(void *varg_pt)//Thread function. Act as an pseudo_SDAQ.
 					arg.pSDAQ_mem->status_send_cnt = Stat_ID_Interval;
 				}
 				arg.pSDAQ_mem->status_send_cnt--;
+				if(arg.pSDAQ_mem->pSDAQ_flags & 1<<info_send)
+				{
+					arg.pSDAQ_mem->pSDAQ_flags &= ~(1<<info_send);
+					p_DeviceInfo(socket_num, arg.pSDAQ_mem->address, arg.pSDAQ_mem->number_of_channels);
+				}
 				if(arg.pSDAQ_mem->pSDAQ_flags & 1<<cal_dates_send)//check if the force send of the cal dates flag is on.
 				{
 					arg.pSDAQ_mem->pSDAQ_flags &= ~(1<<cal_dates_send); //reset force send of the cal dates flag
 					for(int i=0;i<arg.pSDAQ_mem->number_of_channels;i++)
 						p_calibration_date(socket_num, arg.pSDAQ_mem->address, i+1, &(arg.pSDAQ_mem->ch_cal_date[i]));
-				}
-				if(arg.pSDAQ_mem->pSDAQ_flags & 1<<info_send)
-				{
-					arg.pSDAQ_mem->pSDAQ_flags &= ~(1<<info_send);
-					p_DeviceInfo(socket_num, arg.pSDAQ_mem->address, arg.pSDAQ_mem->number_of_channels);
 				}
 			pthread_mutex_unlock(&SDAQs_mem_access[arg.serial_number-arg.start_sn]);
 			// get time and calc different
